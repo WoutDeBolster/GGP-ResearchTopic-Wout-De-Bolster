cd512e10dcd9a61c581256d19cb962f6fc5560e9
nedelpedel <wout.de.bolster@student.howest.be>
2022-01-19 23:24:29 +0100

    adding cost
    there is thill a bug with the overlap code

--- FlowFieldPrototype/Assets/Scripts/FlowField.cs
+++ FlowFieldPrototype/Assets/Scripts/FlowField.cs
@@ -23,13 +23,38 @@ public class FlowField : MonoBehaviour
 
         for (int x = 0; x < m_GridSize.y; x++)
         {
-            for (int y = 0; y < m_GridSize.x; y++)
+            for (int y = 0; y < m_GridSize.y; y++)
             {
                 // for each cell define the wold pos and than adding them in the array (cell radius for an offset to get in the middle)
                 Vector3 worldPos = new Vector3(m_CellDiameter * x + m_CellRadius, 0, m_CellDiameter * y + m_CellRadius);
-                m_Grid[x, y] = new Cell(worldPos, new Vector2Int(x, y));
+                m_Grid[x, y] = new Cell(worldPos, new Vector2Int(x,y));
             }
         }
     }
 
+    public void CreateCostField()
+    {
+        //Vector3 cellHalfExtents = Vector3.one * m_CellRadius;
+        //int terrainMask = LayerMask.GetMask("Impassible", "RoughTerrain");
+        //// going true every cell and if the cell overlaps with a mud ore water block give it the right cost
+        //foreach (Cell curCell in m_Grid)
+        //{
+        //    Collider[] obstacles = Physics.OverlapBox(curCell.m_WorldPos, cellHalfExtents, Quaternion.identity, terrainMask);
+        //    bool hasIncreasedCost = false;
+        //    foreach (Collider col in obstacles)
+        //    {
+        //        if (col.gameObject.layer == 8) // water
+        //        {
+        //            curCell.IncreaseCost(255);
+        //            continue;
+        //        }
+        //        else if (!hasIncreasedCost && col.gameObject.layer == 9) // mud
+        //        {
+        //            curCell.IncreaseCost(3);
+        //            hasIncreasedCost = true;
+        //        }
+        //    }
+        //}
+
+    }
 }
